7dcb5c1195e60398f377c0fd40ce4fbcaad2bb07
Vinski <vinski.andrade@visy.fi>
2024-11-13 14:27:33 -0300

    lights being displayed

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 #include "ButtonClass.h"
 
 const int AMOUNT_OF_STATIONS = 4,
+  AMOUNT_OF_PHASES = 6,
+  PHASE_BLOCK = 15,
   STATIONS[4][2] = {
     {4, 5},
     {6, 7},
@@ -9,7 +12,26 @@ const int AMOUNT_OF_STATIONS = 4,
     {10, 11}
   };
 
-Button buttons[AMOUNT_OF_STATIONS] = {
+unsigned const int LIGHT_TIME_RANGE[2] = {200, 1000};
+
+int currentTarget,
+  currentLap = 0,
+  targets[999],
+  hits[999],
+  variabilityRate = (LIGHT_TIME_RANGE[1] - LIGHT_TIME_RANGE[0]) / AMOUNT_OF_PHASES,
+  phase = 1;
+
+bool gameOn = false,
+  lightOn = false,
+  isVariableLightTimeSet = false;
+
+unsigned int variableLightTime = 0;
+
+unsigned long lightStartingTime = 0;
+
+Button startButton(12);
+
+Button gameButtons[AMOUNT_OF_STATIONS] = {
   Button(STATIONS[0][0]),
   Button(STATIONS[1][0]),
   Button(STATIONS[2][0]),
@@ -20,15 +42,52 @@ void initPins();
 void testLeds();
 void testButtons();
 void blinkAllLights();
+void handleStartButton();
+void sortTarget();
+void initRandomSeed();
 
 void setup() {
   initPins();
   testLeds();
   testButtons();
+  initRandomSeed();
   blinkAllLights();
+  Serial.begin(9600);
 }
 
 void loop() {
+  handleStartButton();
+  if (gameOn) {
+    if (!lightOn) {
+      sortTarget();
+      targets[currentLap] = currentTarget;
+      digitalWrite(STATIONS[currentTarget][1], HIGH);
+      lightStartingTime = millis();
+      lightOn = true;
+    } else {
+      if (!isVariableLightTimeSet) {
+        if (currentLap == 0) {
+          variableLightTime = LIGHT_TIME_RANGE[1];
+        } else {
+          if (currentLap > PHASE_BLOCK * AMOUNT_OF_PHASES) {
+            variableLightTime = LIGHT_TIME_RANGE[0];
+          } else if ((currentLap * phase) % PHASE_BLOCK == 0) {
+            Serial.println();
+            Serial.println(currentLap);
+            variableLightTime = variableLightTime - variabilityRate;
+          }
+        }
+        isVariableLightTimeSet = true;
+      }
+      if (millis() - lightStartingTime > variableLightTime) {
+        digitalWrite(STATIONS[currentTarget][1], LOW);
+        lightOn = false;
+        lightStartingTime = 0;
+        currentLap++;
+        isVariableLightTimeSet = false;
+      }
+    }
+  }
 }
 
 void initPins() {
@@ -51,8 +110,8 @@ void testButtons() {
   for (int i = 0; i < AMOUNT_OF_STATIONS; i++) {
     digitalWrite(STATIONS[i][1], HIGH);
     do {
-      buttons[i].read();
-    } while (!buttons[i].wasPressed());
+      gameButtons[i].read();
+    } while (!gameButtons[i].wasPressed());
     digitalWrite(STATIONS[i][1], LOW);
   }
 }
@@ -88,4 +147,26 @@ void blinkAllLights() {
   digitalWrite(STATIONS[2][1], LOW);
   digitalWrite(STATIONS[3][1], LOW);
   delay(200);
+}
+
+void handleStartButton() {
+  startButton.read();
+  if (startButton.wasPressed()) {
+    gameOn = !gameOn;
+  }
+}
+
+void sortTarget() {
+  int lastTarget = currentTarget;
+  do {
+    currentTarget = random(AMOUNT_OF_STATIONS);
+  } while (lastTarget == currentTarget);
+}
+
+void initRandomSeed() {
+  size_t const address {0};
+  unsigned int seed {};
+  EEPROM.get(address, seed);
+  randomSeed(seed);
+  EEPROM.put(address, seed + 1);
 }
