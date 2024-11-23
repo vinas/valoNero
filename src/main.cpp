b8c2f5956048094958720b1537bf2c4f8c526e70
Vinski <vinski.andrade@visy.fi>
2024-11-23 14:50:52 -0300

    even more refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <EEPROM.h>
 #include "ButtonClass.h"
 
-const int AMOUNT_OF_STATIONS = 4,
+const int MAX_RESULTS = 999;
+
+const byte AMOUNT_OF_STATIONS = 4,
   START_BTN_PIN = 12,
   GAME_LED_PIN = 13,
-  MAX_RESULTS = 500,
   STATIONS[4][2] = {
     {4, 5},
     {6, 7},
@@ -13,9 +14,10 @@ const int AMOUNT_OF_STATIONS = 4,
     {10, 11}
   };
 
-int lightLap,
-  buttonLap,
-  targets[MAX_RESULTS];
+short lightLap,
+  buttonLap;
+
+byte targets[MAX_RESULTS];
 
 bool gameOn, lightOn;
 
@@ -42,13 +44,13 @@ void initRandomSeed();
 unsigned long calcCurrentLightDuration(int lap);
 void handleGameButtons();
 void sortAllStations();
-
+void printResults();
 
 void setup() {
+  Serial.begin(9600);
   initPins();
   testButtons();
   initRandomSeed();
-  resetGameProps();
   blinkAllLights();
 }
 
@@ -100,9 +102,8 @@ void blinkAllLights() {
 }
 
 void resetGameProps() {
-  gameOn = false;
   lightLap = -1;
-  buttonLap = -1;
+  buttonLap = 0;
   lightOn = false;
   sortAllStations();
 }
@@ -119,13 +120,15 @@ void handleStartButtonPress() {
 }
 
 void handleStartGame() {
-  gameOn = true;
+  resetGameProps();
   digitalWrite(GAME_LED_PIN, HIGH);
+  gameOn = true;
 }
 
 void handleEndGame() {
+  gameOn = false;
   blinkAllLights();
-  resetGameProps();
+  printResults();
 }
 
 void handleGameLights() {
@@ -188,9 +191,9 @@ void handleGameButtons() {
   for (int i = 0; i < AMOUNT_OF_STATIONS; i++) {
     gameButtons[i].read();
     if (gameButtons[i].wasPressed()) {
-      buttonLap++;
       if (targets[buttonLap] == i) {
         if (buttonLap == lightLap) digitalWrite(STATIONS[targets[lightLap]][1], LOW);
+        buttonLap++;
         return;
       }
       handleEndGame();
@@ -204,4 +207,10 @@ void sortAllStations() {
       targets[i] = random(AMOUNT_OF_STATIONS);
     } while (i > 0 && targets[i - 1] == targets[i]);
   }
+}
+
+void printResults() {
+  Serial.println();
+  Serial.print("buttonLap: ");
+  Serial.println(buttonLap);
 }
